754f0fd1de2ce6c97facf2e1ca2c6ec6b5064bb3
nhanbui1512 <buithiennhan0345@gmail.com>
2023-03-11 21:04:57 +0700

    add view admin page ( addProduct, createAccount, listStaffAcc, listCustmAcc) + add LOG setting

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -133,3 +133,25 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+# Cấu hình log  
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+    },
+}
--- admin/urls.py
+++ admin/urls.py
@@ -3,4 +3,9 @@ from . import admincontroller
 
 urlpatterns = [
     path("", admincontroller.index),
+    path("addproduct/", admincontroller.addProduct),
+    path("staffaccounts/",admincontroller.staffAccount),
+    path("customeraccounts/",admincontroller.customerAccount),
+    path("createaccount/",admincontroller.createAccount),
+    
 ]
