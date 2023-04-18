413efa12b63cff12a610e3a1f9334c11448e9b45
nhanbui1512 <buithiennhan0345@gmail.com>
2023-04-18 14:26:31 +0700

    handle cart

--- PythonWeb/middleware.py
+++ PythonWeb/middleware.py
@@ -15,14 +15,14 @@ def LoginRequiredMiddleware(get_response):
     #     return response
     
     def middleware(request):
-        print(request.session.get('id'))
+        # print(request.session.get('id'))
         if ('admin/' in request.build_absolute_uri()) and (request.session.get('id') == None):
             return redirect('Home:Login')
-        print(request.headers)
+        # print(request.headers)
         if 'Referer' in request.headers:
             pass
         else:
-            print('khong')
+            # print('khong')
             if not ('admin/' in request.path) and request.session.get('id') != None:
                 del request.session['id']
         response = get_response(request)
